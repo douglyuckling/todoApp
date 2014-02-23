f92a464aaa3367eed4548815175cf0d08ffcf335
Douglas Paul <douglas.w.paul@gmail.com>
2014-02-23 10:22:03 -0500

    Log exceptions when handling requests

--- web/src/main/groovy/bmug/todoapp/web/controller/TasksController.groovy
+++ web/src/main/groovy/bmug/todoapp/web/controller/TasksController.groovy
@@ -2,13 +2,18 @@ package bmug.todoapp.web.controller
 
 import bmug.todoapp.domain.Task
 import bmug.todoapp.services.TaskService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
 import static org.springframework.web.bind.annotation.RequestMethod.DELETE
 import static org.springframework.web.bind.annotation.RequestMethod.GET
 import static org.springframework.web.bind.annotation.RequestMethod.POST
@@ -17,6 +22,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.PUT
 @Controller
 @RequestMapping('/data/tasks')
 class TasksController {
+    private static Logger LOG = LoggerFactory.getLogger(TasksController)
 
     @Autowired
     private TaskService taskService
@@ -43,6 +49,12 @@ class TasksController {
     @ResponseBody
     void deleteTask(@PathVariable('taskId') String taskId) {
         taskService.deleteTaskById(taskId)
+    }
+
+    @ExceptionHandler(Exception)
+    ResponseEntity<String> handleException(Exception e) {
+        LOG.error("Failed while handling request", e)
+        return new ResponseEntity<String>(INTERNAL_SERVER_ERROR)
     }
 
 }
